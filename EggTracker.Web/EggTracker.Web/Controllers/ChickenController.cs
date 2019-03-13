80a209842a0b8fa13033c1eaf19379db47eb2e59
David Powers <DPowers@newgistics.com>
2019-03-12 19:53:32 -0500

    init commit

---
+++ EggTracker.Web/EggTracker.Web/Controllers/ChickenController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EggTracker.Web.Models.Chicken;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+
+namespace EggTracker.Web.Controllers
+{
+    public class ChickenController : Controller
+    {
+        private readonly IMongoCollection<ChickenModel> _chickens;
+
+        public ChickenController(IConfiguration config)
+        {
+            var client = new MongoClient(config.GetConnectionString("EggTracker"));
+            var database = client.GetDatabase("EggTracker");
+            _chickens = database.GetCollection<ChickenModel>("Chickens");
+        }
+
+        public IActionResult Create()
+        {
+            var model = new ChickenModel();
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(ChickenModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                await _chickens.InsertOneAsync(model);
+            }
+
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> ListChicken()
+        {
+            ListChickenModel model = new ListChickenModel
+            {
+                Chickens = new List<ChickenModel>()
+            };
+
+            var findings = await _chickens.FindAsync(chicken => true);
+            model.Chickens = findings.ToList();
+
+
+            return View(model);
+        }
+
+        public async Task<IActionResult> Delete(string id)
+        {
+            await _chickens.DeleteOneAsync(chicken => chicken.Id == id);
+            return RedirectToAction("ListChicken");
+        }
+    }
+}
---
+++ EggTracker.Web/EggTracker.Web/Models/Chicken/ChickenModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace EggTracker.Web.Models.Chicken
+{
+    public class ChickenModel
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [Required]
+        [Display(Name="Name")]
+        [BsonElement("Name")]
+        public string Name { get; set; }
+
+        [Required]
+        [Display(Name = "Breed")]
+        [BsonElement("Breed")]
+        public string Breed { get; set; }
+
+        [Required]
+        [DataType(DataType.DateTime)]
+        [Display(Name = "DOB")]
+        [BsonElement("Dob")]
+        public DateTime Dob { get; set; }
+    }
+}
